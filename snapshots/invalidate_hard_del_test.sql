b5bf48ab085cde2f9cab28164f65a051d91d8abd
Kalaivani Ravi <kalaivaniravi2208@gmail.com>
2025-07-07 05:13:32 +0000

    final commit

--- snapshots/invalidate_hard_del_test.sql
+++ snapshots/invalidate_hard_del_test.sql
@@ -1,11 +1,11 @@
-{% snapshot test_newrecord_snapshot %}
+{% snapshot test_invalidate_snapshot %}
     {{
         config(
             target_schema="ss",
             unique_key="id",
             strategy="timestamp",
             updated_at="updated_at",
-            hard_deletes="new_record",
+            hard_deletes="invalidate",
         )
     }}
 
